66bb5a5d5c5c843c3aa97c011a9c17766759ea3c
Zak Werdegar <zakwe@DNa1d05cd.SUNet>
2024-02-10 18:30:11 -0800

    clear scraping files

--- src/main.py
+++ src/main.py
@@ -2,12 +2,23 @@ from url_scraper import *
 from url_data_collect import *
 from graph import *
 
+def setup_files():
+    # Open and clear scraped_text.txt
+    with open('scraped_text.txt', 'w') as file:
+        pass  # Opening in 'w' mode and closing the file clears it.
+
+    # Open and clear url_data.txt
+    with open('url_data.txt', 'w') as file:
+        pass  # Similarly, this clears the file.
 
 def main():
+    setup_files()
     url = input("Enter the webpage URL: ")
     html_content = fetch_page_content(url)
     
     if html_content:
+        
+        
         # Scrape the text
         text = extract_text_from_html(html_content)
         save_text_to_file(text)
